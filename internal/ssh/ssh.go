e4fb2bb7f438d8251704b9afa64bb995cef3a16f
TY <47445484+R-YaTian@users.noreply.github.com>
2025-09-22 15:20:53 +0800

    Support msys2-git (NOT git for windows)

--- internal/ssh/ssh.go
+++ internal/ssh/ssh.go
@@ -13,6 +13,7 @@ import (
 	"path/filepath"
 	"strings"
 	"syscall"
+	"runtime"
 
 	"github.com/pkg/errors"
 	"github.com/sourcegraph/run"
@@ -186,6 +187,9 @@ func setupHostKeys(appDataPath string, algorithms []string) ([]ssh.Signer, error
 	var hostKeys []ssh.Signer
 	for _, algo := range algorithms {
 		keyPath := filepath.Join(dir, "gogs."+algo)
+		if runtime.GOOS == "windows" {
+			keyPath = strings.ReplaceAll(keyPath, "\\", "/")
+		}
 		if !osutil.IsExist(keyPath) {
 			args := []string{
 				conf.SSH.KeygenPath,
